faadebfb8593fc4efcae2a04990fb743eb5e3c38
Joe Wolfe <JoeWolfe0527@gmail.com>
2024-03-03 23:10:29 -0500

    finished exercise

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,7 @@
+const Router = require("express").Router;
+const router = new Router();
+
+const User = require("../models/user");
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,6 +14,17 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get('/:id', async function (req, res, next) {
+    try {
+        const message = await User.get(req.params.id);
+        if (message.from_user.username !== req.user.username && message.to_user.username !== req.user.username) {
+            throw new ExpressError("Unauthorized", 401);
+        }
+        return res.json({ message });
+    } catch (err) {
+        return next(err);
+    }
+});
 
 
 /** POST / - post message.
@@ -18,6 +33,14 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post('/', async function (req, res, next) {
+    try {
+        const message = await User.create(req.body);
+        return res.json({ message });
+    } catch (err) {
+        return next(err);
+    }
+});
 
 
 /** POST/:id/read - mark message as read:
@@ -27,4 +50,16 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+router.post('/:id/read', async function (req, res, next) {
+    try {
+        const message = await User.markRead(req.params.id);
+        if (message.to_user.username !== req.user.username) {
+            throw new ExpressError("Unauthorized", 401);
+        }
+        return res.json({ message });
+    } catch (err) {
+        return next(err);
+    }
+});
 
+module.exports = router;
